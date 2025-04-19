76d077d6f8a9f81ac5b5c447f3c5e6acb4ebd257
eyadmomen <eyad.momen1014@gmail.com>
2025-04-19 17:14:54 +0200

    Update: added new route / improved MongoDB connection

--- src/initapp.js
+++ src/initapp.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose"
-
+import cors from "cors"
 import { feedbackRouter, userRouter } from "./allroutes.js"
-
-
 export const initapp = (app, express)=>{
-    const port =  5000
+    const port =  process.env.PORT
 app.use(express.json())
+app.use(cors())
 app.use('/user', userRouter)
 app.use('/feedback', feedbackRouter)
 app.all('*', (req, res, next) =>
     res.status(404).json({ message: '404 Not Found URL' }),
   )
+
 mongoose.connect(process.env.DB_URL || "").then(()=>{
   console.log("connected to database")
 })
